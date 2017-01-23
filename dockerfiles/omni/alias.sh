ecb67da2310b1fcfb9ba6ed07ae6d92659ab46e4
y12studio <y12studio@gmail.com>
2017-01-23 12:22:31 +0800

    added omni test

---
+++ dockerfiles/omni/alias.sh
@@ -0,0 +1,10 @@
+DNAME=y12docker/dltdojo-omni
+alias build='docker build -t ${DNAME} . ; echo "docker build -t ${DNAME}";  docker images | grep ${DNAME}'
+alias drun='docker run -it ${DNAME}'
+PNAME=xcp
+DCNAME=dev${PNAME}
+alias dc='docker-compose -p $DCNAME -f peers.yml'
+alias dcend='dc stop ; dc rm -f'
+alias dcup='dcend ; dc up -d; dc ps'
+alias dexec1='docker exec -it ${DCNAME}_${PNAME}1_1'
+alias cli1='dexec1 omnicore-cli'
---
+++ dockerfiles/omni/testlog.md
@@ -0,0 +1,10 @@
+### 2017-01-23T12:17:02+0800
+```
+$ build
+$ drun omnicored --version
+Omni Core daemon version v0.0.11.2
+Copyright (C) 2013-2016 The Omni Core Developers
+Copyright (C) 2009-2016 The Bitcoin Core Developers
+$ drun omnicore-cli --version
+Omni Core RPC client version v0.0.11.2
+```
